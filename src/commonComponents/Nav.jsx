5664f972afe18ffc3a34ce1fb6ee02f558d6b650
Rafael Menezes <rafaelsm.me@gmail.com>
2021-12-21 22:57:34 +1000

    update workflow to inject secret into a json config file

--- src/commonComponents/Nav.jsx
+++ src/commonComponents/Nav.jsx
@@ -5,6 +5,7 @@ import {
   faPlus,
 } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import config from "globalConfig.json";
 
 function Nav({ title, disableAdd }) {
   return (
@@ -14,6 +15,22 @@ function Nav({ title, disableAdd }) {
           <FontAwesomeIcon icon={faBroadcastTower} size="lg" className="mr-3" />
           {title}
         </Link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item text-light">{config.apiUrl}</li>
+          </ul>
+        </div>
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
             <Link
