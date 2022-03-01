cec7e1066ed66da754b61b1c761369f1a0235e5c
Rafael Menezes <rafaelsm.me@gmail.com>
2022-03-01 23:19:45 +1000

    Fix hook import

--- src/commonComponents/Nav.jsx
+++ src/commonComponents/Nav.jsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBroadcastTower, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import { useAuth } from "Auth/Auth";
-import { useState } from "react/cjs/react.development";
+import { useState } from "react";
 
 function Nav({ title, disableAdd }) {
   const [show, setShow] = useState(false);
